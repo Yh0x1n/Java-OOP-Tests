4d9d1e40c926f97c8a5f8276a61dbfa92dc0a104
Yh0x1n <95248471+Yh0x1n@users.noreply.github.com>
2021-12-01 04:51:00 -0800

    Update MainPOO.java

--- Proyectos_POO/MainPOO.java
+++ Proyectos_POO/MainPOO.java
@@ -6,14 +6,9 @@ public class MainPOO {
     public static void main(String[] args) {
         TestClass A = new TestClass(); //Object created from TestClass class.
         int [] array = new int [5]; //Array
-        for (int i = 1; i <= 5; i++) {
-            array[i] = Integer.parseInt(JOptionPane.showInputDialog("Insert " + i + " numbers, please."));  
-            // I get an error here while running the program.
-            
-            //"Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 
-            //Index 5 out of bounds for length 5 at Proyectos_POO.MainPOO.main(MainPOO.java:10)"
+        for (int i = 0; i <= 4; i++) {
+            array[i] = Integer.parseInt(JOptionPane.showInputDialog("Insert " + (i+1) + " numbers, please."));
         }
-       
-        A.arregloInverso(array);
+        A.arregloInverso(array); //Calling the class on TestClass.
     }
 }
