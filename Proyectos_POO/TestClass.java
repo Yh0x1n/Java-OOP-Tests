cb66aad91a34939581a566df9fc1d8b7a2cc355c
Yh0x1n <95248471+Yh0x1n@users.noreply.github.com>
2021-12-01 04:48:48 -0800

    Update TestClass.java

--- Proyectos_POO/TestClass.java
+++ Proyectos_POO/TestClass.java
@@ -1,15 +1,11 @@
 package Proyectos_POO;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 import javax.swing.*;
 
 public class TestClass {
     public void arregloInverso(int [] array2) {
-        JOptionPane.showMessageDialog(null, "Array has been reverted."); 
-        Collections.reverse(Arrays.asList(array2));//Reverting the array with Collections.
-        for (int i = 0; i <= 5; i++) {
+        JOptionPane.showMessageDialog(null, "Array has been reverted.");
+        for (int i = 4; i >= 0; i--) {
 
             JOptionPane.showMessageDialog(null, array2[i]);//Shows reverted array.
         }
